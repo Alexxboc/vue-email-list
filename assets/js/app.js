4509b3c2980bbba3063302c013fa85503bb265e7
Alexxboc <boccardi.alessandro@gmail.com>
2022-04-29 17:04:00 +0200

    add comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,22 +12,23 @@ const app = new Vue ({
 
     data: {
 
-        email: '',
+        email: '', // Definisco una proprietà per la singola mail
 
-        emails: []
+        emails: [] // Definisco un array vuoto per le mail
     },
 
     methods: {},
 
     mounted() {
         
+        // Ciclo per generare 10 mail
         for (let index = 0; index < 10; index++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then(response => {
                 // console.log(this);
                 // console.log(response);
-                this.email = response.data.response
-                this.emails.push(this.email)
+                this.email = response.data.response 
+                this.emails.push(this.email) // "Pusho" all'interno dell'array emails le singole mail
             })
             
             // console.log(this.emails);

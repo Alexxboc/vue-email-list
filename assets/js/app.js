2782ef113dc1c87ef12b85ae33953d25f56096e8
Alexxboc <boccardi.alessandro@gmail.com>
2022-04-29 16:46:24 +0200

    complete ex

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,40 @@
+/* 
+Descrizione:
+Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
+
+
 const app = new Vue ({
     el: '#app',
 
-    data: {},
+    data: {
+
+        email: '',
+
+        emails: []
+    },
 
     methods: {},
 
-    mounted() {}
+    mounted() {
+        
+        for (let index = 0; index < 10; index++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+                // console.log(this);
+                // console.log(response);
+                this.email = response.data.response
+                this.emails.push(this.email)
+            })
+            
+            // console.log(this.emails);
+        }
+        
+         
+     }
+            
+           
 }) 
